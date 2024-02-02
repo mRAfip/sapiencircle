0a9848703091776e079a3abeb089593ab2dce986
mRAfip <rafipmkm@gmail.com>
2024-02-02 21:48:20 +0530

    add template page

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2793,6 +2793,15 @@
 	});
 
 
+		// Hover scrolling speed.
+		$(".tt-scrolling-work").each(function() {
+			var $tt_stSpeed = $(this).data("scroll-speed");
+			$(this).find(".tt-scrolling-work-inner").css({ 
+				"animation-duration": $tt_stSpeed + "s",
+			});
+		});
+	
+
 
 	// ================================================================
 	// Scroll between anchors 
